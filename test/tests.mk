7298d7614fb15b3c7cd8f768e74a49ace409cc4d
Matt Palmer <mpalmer@hezmatt.org>
2011-01-22 15:22:44 +1100

    Add ethtool-lite.o to test build objects, so things build

--- test/tests.mk
+++ test/tests.mk
@@ -6,7 +6,7 @@ TESTS = test/test_inet_mton.o						\
         test/test_netcfg_gateway_reachable.o
 
 # List of other objects that we need to pull in to make the tests work
-OBJECTS = netcfg-common.o wireless.o
+OBJECTS = netcfg-common.o wireless.o ethtool-lite.o
 
 test/run: $(TESTS) $(OBJECTS) test/srunner.o
 	$(CC) -o $@ $^ $(LDOPTS) -lcheck
