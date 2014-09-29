aa8b171d3ed36cc038734822dd0c8e4e8272b427
Aurelien Jarno <aurelien@aurel32.net>
2014-09-29 07:02:48 +0200

    netcfg-static.c: call netcfg_interface_init() on struct netcfg_interface before using it to avoid calling free() on a random pointer.

--- netcfg-static.c
+++ netcfg-static.c
@@ -38,6 +38,7 @@ int main(int argc, char** argv)
 
     /* initialize libd-i */
     di_system_init("netcfg-static");
+    netcfg_interface_init(&interface);
     if (strcmp(basename(argv[0]), "ptom") != 0)
         di_info("Starting netcfg v.%s (built %s)", NETCFG_VERSION, NETCFG_BUILD_DATE);
 
